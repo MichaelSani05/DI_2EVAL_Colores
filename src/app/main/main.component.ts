0d2b783c1d30c2051c26ebaae9ec2efe5a33a97d
MichaelSani05 <msmichaelsani@gmail.com>
2025-01-15 10:23:02 +0100

    V-0.0.6

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef} from '@angular/core';
 import { SliderComponent } from "../slider/slider.component";
+import { ThemeService } from '../services/theme.service';
 
 @Component({
   selector: 'app-main',
@@ -8,5 +9,31 @@ import { SliderComponent } from "../slider/slider.component";
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  mode: string = ''
+  constructor(private renderer: Renderer2, private themeService: ThemeService, private el: ElementRef){}
+
+  ngOnInit(): void {
+    this.themeService.mode$.subscribe((mode) => {
+      this.mode = mode;
+      if (mode == 'occidental') {
+        this.updateBackgroundImage("../../assets/images/banner.webp");
+      } else if (mode == 'oriental') {
+        this.updateBackgroundImage("../../assets/images/banner-3.jpg");
+      }
+    });
+  }
+
+  updateBackgroundImage(imageUrl: string): void {
+    const bannerElement = this.el.nativeElement.querySelector('#banner');
+    if (bannerElement) {
+      this.renderer.setStyle(
+        bannerElement,
+        'background-image',
+        `url(${imageUrl})`
+      );
+    } else {
+      console.warn('Banner element not found!');
+    }
+  }
 
 }
